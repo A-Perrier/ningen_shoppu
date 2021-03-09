91490f3ae86455ab3c376f4d95e3d7f28405b9ae
Anthony Perrier <perrier_anthony@live.fr>
2021-03-09 08:23:08 +0100

    [ADD] Création des entités Purchase et PurchaseItem

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -68,9 +68,15 @@ class Product
      */
     private $isOnSale;
 
+    /**
+     * @ORM\OneToMany(targetEntity=PurchaseItem::class, mappedBy="product")
+     */
+    private $purchaseItems;
+
     public function __construct()
     {
         $this->productImages = new ArrayCollection();
+        $this->purchaseItems = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -188,6 +194,36 @@ class Product
     public function setIsOnSale(bool $isOnSale): self
     {
         $this->isOnSale = $isOnSale;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|PurchaseItem[]
+     */
+    public function getPurchaseItems(): Collection
+    {
+        return $this->purchaseItems;
+    }
+
+    public function addPurchaseItem(PurchaseItem $purchaseItem): self
+    {
+        if (!$this->purchaseItems->contains($purchaseItem)) {
+            $this->purchaseItems[] = $purchaseItem;
+            $purchaseItem->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removePurchaseItem(PurchaseItem $purchaseItem): self
+    {
+        if ($this->purchaseItems->removeElement($purchaseItem)) {
+            // set the owning side to null (unless already changed)
+            if ($purchaseItem->getProduct() === $this) {
+                $purchaseItem->setProduct(null);
+            }
+        }
 
         return $this;
     }
